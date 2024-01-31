046cdbea2cd2d14410976883f16bd3bbb4e17d4f
dmunozdelrio <dmunozdelrio@gmail.com>
2024-01-31 16:24:29 -0600

    lead creation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.querySelectorAll('.btn');
+var btnContact = document.getElementById('send');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -20,8 +21,35 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// When the user clicks on the button of the modal it send the data to an api and close the modal saying that the data was sent
+btnContact.onclick = function() {
+    var name = document.getElementsByName('name')[0].value;
+    // var email = document.getElementById('email').value;
+    // var message = document.getElementById('message').value;
+    var data = {
+        name: name,
+        // email: email,
+        // message: message
+    };
+    fetch('http://localhost:3000/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
         modal.style.display = "none";
-    }
+        alert('Data sent');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
